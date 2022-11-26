40e6b87619283861577bb279fb5647f7f472003b
gustavorsan <gustavorsantos42@gmail.com>
2022-11-26 17:07:23 -0300

    Adcionado variaveis ao componente notificationList M1S08E07

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'ntp-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloAngular';
+  title = 'NotificationApp';
+ 
+  constructor(private titleService:Title) {
+  }
+ 
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+  }
 }
--- src/app/notification-list/notification-list.component.ts
+++ src/app/notification-list/notification-list.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'ntp-notification-list',
   templateUrl: './notification-list.component.html',
   styleUrls: ['./notification-list.component.css']
 })
 export class NotificationListComponent {
-
+  notifications = [
+    {
+      title: 'Atenção',
+      description: 'A data de entrega é até o dia 04-11-2022',
+      date: '01-11-2022'
+    },
+    {
+      title: 'Atenção',
+      description:  'A data de entrega é até o dia 04-11-2022',
+      date: '03-11-2022'
+    },
+    {
+      title: 'Aviso',
+      description: 'projeto entregue com sucesso',
+      date: '04-11-2022'
+    }
+  ]
 }
