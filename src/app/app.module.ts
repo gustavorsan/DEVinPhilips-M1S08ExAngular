a041a11ac51ae50d6799f955fb095d492e1b615e
gustavorsan <gustavorsantos42@gmail.com>
2022-11-26 14:52:48 -0300

    Criação da navbar M1S08E04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotificationListComponent
+    NotificationListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
